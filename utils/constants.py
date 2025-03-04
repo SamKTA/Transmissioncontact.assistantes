3d13ce1d95119f29067d0e6b420d714e3fc583b9
SamKTA <150455266+SamKTA@users.noreply.github.com>
2025-03-04 15:58:07 +0100

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -55,8 +55,8 @@ ASSISTANTES = ["Laura", "Léonor", "Autre"]
 
 # Configuration pour l'envoi d'emails
 EMAIL_CONFIG = {
-    "sender": "skita@orpi.com",
-    "smtp_server": "smtp.orpi.com",
+    "sender": "contactpro.skdigital@gmail.com",
+    "smtp_server": "smtp.gmail.com",
     "port": 587,
     "subject_prefix": "🔔 +1 CONTACT : ",
     "sender_name": "Transmission Contact ORPI Arcades"
